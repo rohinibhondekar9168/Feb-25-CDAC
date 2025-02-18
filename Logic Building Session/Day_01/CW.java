efac88a2ff601f512d65165c547961180cbd18a7
Rohini Bhondekar <163813369+rohinibhondekar9168@users.noreply.github.com>
2025-02-18 21:29:02 +0530

    Add files via upload

---
+++ Logic Building Session/Day_01/CW.java
@@ -0,0 +1,45 @@
+class CW{
+public static void main(String[]args)
+{
+//System.out.println("Welcome to PGDAC course Feb-25...!!!");
+
+/*
+ int a=10;
+ System.out.println("Value of a = "+a);
+ System.out.println(a);
+ System.out.println(a+a);
+*/
+//byte a=131;
+// System.out.println(a); // error: incompatible types: possible lossy conversion from int to byte
+
+//byte a = (byte)-128;
+//System.out.println(a); 
+
+/*
+int a=10;
+int a1;
+
+short s = (byte)a;
+System.out.println(s); //10
+
+*/
+
+/*
+float a=10.0f;
+int f=(int)a;
+System.out.println(f);//10
+*/
+
+/*
+double f1=10.0;
+long l1=(long)f1;
+System.out.println(l1);//10
+*/
+
+int a=10;
+float f=a;
+System.out.println(f);
+
+
+}
+}
